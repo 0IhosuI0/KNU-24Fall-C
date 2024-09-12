dac856e21fedd4586770bb53c9a6843fb29c1af1
hosu10 <ghtn10000@naver.com>
2024-09-12 13:25:12 +0900

    day02-1 1.0ver

---
+++ day02/day02-1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int func;
+    double num1, num2, result;
+
+    printf("사칙연산 프로그램 \n");
+    printf("원하는 기능을 입력하시오. \n");
+    printf("1. 더하기, 2. 뺴기, 3. 곱하기, 4. 나누기 \n");
+
+    printf("기능 : ");
+    scanf("%d",&func);
+
+    printf("숫자 1을 입력하세요 : ");
+    scanf("%lf", &num1);
+
+    printf("숫자 2룰 입력하세요 : ");
+    scanf("%lf", &num2);
+
+    if(func == 1){
+        result = num1 + num2;
+        printf("%lf + %lf = %lf \n", num1, num2, result);
+    }
+    else if(func == 2){
+        result = num1 - num2;
+        printf("%lf - %lf = %lf \n", num1, num2, result);
+    }
+    else if(func == 3){
+        result = num1 * num2;
+        printf("%lf * %lf = %lf \n", num1, num2, result);
+    }
+    else if(func == 4){
+        result = num1 / num2;
+        printf("%lf / %lf = %lf \n", num1, num2, result);
+    }
+    else{
+        printf("잘못된 값입니다. 프로그램을 다시 실행해주십시오. \n");
+        return 0;
+    }
+
+    return 0;
+    
+}
