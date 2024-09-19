dce38054ef43555c75fe149a819ed0933717b5fe
hosu10 <ghtn10000@naver.com>
2024-09-19 14:37:18 +0900

    ver 1.1

--- day03/day03-2.c
+++ day03/day03-2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int Pac(int num_r);
+int Fac(int num_r);
 
 int main(){
     int num_t, result;
 
     printf("계산할 팩토리얼 값 입력 : ");
     scanf("%d", &num_t);
-    result = Pac(num_t);
+    result = Fac(num_t);
     printf("%d! = %d \n", num_t, result);
 
     return 0;
 }
 
-int Pac(int num_r){
+int Fac(int num_r){
     int pac_num;
     
     if(num_r == 1){
@@ -23,7 +23,7 @@ int Pac(int num_r){
         pac_num = 1;
     }
     else{
-        pac_num = num_r*Pac(num_r - 1);
+        pac_num = num_r*Fac(num_r - 1);
     }
 
     return pac_num;
