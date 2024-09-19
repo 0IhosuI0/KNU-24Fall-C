3215ee335ca1b718b78301ccefac951cd2d5c481
hosu10 <ghtn10000@naver.com>
2024-09-19 13:02:32 +0900

    ver 1.3

--- day02/day02-3.c
+++ day02/day02-3.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int i;
+    int i, cnt = 0;
 
     for(i = 0; i < 100; i++){
         if(i % 12 == 0 || i % 7 == 0){
-            printf("%d \n", i);
+            printf("%d \t", i);
+            cnt++;
         }
+
+        if (cnt % 6 == 0){
+            printf("\n");
+        }
+        
+        
 
     }
 
