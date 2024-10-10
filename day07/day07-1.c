15dd3985daaf232887d9bb89a063f1bcdde433c9
hosu10 <ghtn10000@naver.com>
2024-10-10 13:37:31 +0900

    ver 1.1

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -4,18 +4,18 @@ int main(){
     int num;
 
     printf("몇 층 피라미드를 만드시겠습니까? : ");
-    scanf("%d", &num);
+    scanf("%d", &num); //피라미드의 층 수를 지정
     printf("%d층 피라미드\n");
     
-    for(int i = 1; i <= num; i++){
-        for (int j = 0; j <= num-i; j++){
+    for(int i = 1; i <= num; i++){ //n층의 피라미드를 만들기 위한 for문
+        for (int j = 0; j <= num-i; j++){ //가운데 정렬의 피라미드를 만들기 위해 num-i만큼의 공백을 출력
             printf(" ");
         }
         
-        for(int v = 1; v <= i*2-1; v++){
+        for(int v = 1; v <= i*2-1; v++){ //한 층당 홀수 개의 *을 출력, 층이 늘어날 때 마다  *의 개수가 2개씩 늘어남
             printf("*");
         }
-        printf("\n");
+        printf("\n"); //다음 층으로 줄 넘김
     }
     
     return 0;
--- day07/day07-2.c
+++ day07/day07-2.c
@@ -3,41 +3,43 @@
 #include <time.h>
 #include <math.h> 
 
-int inCircle(int x, int y, int r);
 
 int main(){
-    double x = 0;
+    double x = 0; 
     double y = 0;
     double PI;
-    int count = 1, circle = 0, cnt = 0;
+    int count = 1, circle = 0, cnt = 0; 
+    //count : 난수를 생성하고 원 안에 있는 점을 구한 횟수
+    //cnt : 실제 계산 횟수
+    //circle : 원 안의 점의 갯수
     
-     srand(time(NULL));
+     srand(time(NULL)); //rand의 seed값을 시간에 따라 변하게 조정
     
-    for(int i = 1; i <= 100; i++){
-       while (count <= 10000000){
-            x = (double)rand() / (double)RAND_MAX;
-            y = (double)rand() / (double)RAND_MAX;
-            if (sqrt(pow(x, 2) + pow(y, 2)) < 1){
-                circle++;
+    for(int i = 1; i <= 100; i++){ //100번의 반복으로 1%~100% 까지 구현
+       while (count <= 10000000){  //10억번 반복하기위해 100*100만번의 실행
+            x = (double)rand() / (double)RAND_MAX; //x의 값을 0~1 사이의 실수로 랜덤하게 지정
+            y = (double)rand() / (double)RAND_MAX; //y의 값을 0~1 사이의 실수로 랜덤하게 지정
+            if (sqrt(pow(x, 2) + pow(y, 2)) < 1){ //sqrt(pow(x, 2)+(pow(y, 2)) < 1)을 만족하면 원 안에 있는 점
+                circle++; //원안에 있는 점 을 의미하는 변수를 +1 함
             }
             count ++;
-            cnt ++;
-        }
-        count = 0;
-        PI = 4 * ((double)circle / (double)cnt);
-        printf("%d%% 진행.. 원주율 : %lf",i , PI);
-        printf("{");
-        for(int j = 0; j < 20; j++){
-            if((i / 5) > j){
+            cnt ++; //count 변수가 100만번마다 초기화 되므로 새로운 카운트 값을 1씩 증가
+         }
+        count = 1; //100만번을 100번 돌리므로 100만번의 while문 실행 후 count 값을 0 으로 초기화
+        PI = 4 * ((double)circle / (double)cnt); //PI = 4*(원 안의 점 / 생성한 점)임을 이용하여 계산
+        printf("%3d%% 진행.. 원주율 : %lf  ",i , PI); //계산중인 원주율 표시
+        printf("{"); //진행바 시작 {                    }
+        for(int j = 0; j < 20; j++){ //5% 마다 진행바가 한칸씩 늘어나므로 20회로 나누어 연산
+            if((i / 5) > j){ //5%마다 =이 한개씩 더 출력
                 printf("=");
             }
-            else{
+            else{ // 나머지 칸의 공백
                 printf(" ");
             }
         }
         printf("}\n");
     }
 
-    printf("\n원주율 : %lf\n", PI);
+    printf("\n원주율 : %lf\n", PI); //원주율 최종 계산 값 출력
     return 0;
 }
