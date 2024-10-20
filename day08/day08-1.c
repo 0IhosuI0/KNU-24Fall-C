36f99a9f306ae9eb64b68a47a662078bd9dd56bc
hosu10 <ghtn10000@naver.com>
2024-10-20 17:23:34 +0900

    ver 1.2

--- day08/day08-1.c
+++ day08/day08-1.c
@@ -8,15 +8,19 @@ void revert_Str(char* rev_Str);
 int OK = 0;
 
 int main(){
-    char* character;
-    character = (char*)malloc(sizeof(char));
-
+    char* character; 
+    character = (char*)malloc(sizeof(char)); 
+    //문자열 포인터 선언
+    //동적 할당하며 포인터를 초기화 
     printf("문자열을 입력해 주세요 : ");
     scanf("%s", character, 100);
     Str_Err(character);
+    //Str_Err() 함수에서 문자열에 공백, 특수문자의 유무를 확인
+    //공백이나 특수문자가 없다면 문자열을 뒤집는 함수 실행
 
     free(character);
     character = NULL;
+    //메모리 동적 할당을 해제하고 포인터를 초기화
 
     return 0;
 }
@@ -27,12 +31,16 @@ void Str_Err(char* str){
             OK++;
         }
     }
+    //문자열 길이만큼 한글자씩 특수문자가 있는지 확인
+    //특수문자가 아니라면 OK변수를 1씩 증가
     if (OK == strlen(str)){
         revert_Str(str);
     }
     else{
         printf("공백과 특수문자는 허용되지 않습니다! 영어를 입력해주세요! \n");
     }
+    //OK변수의 값이 문자열의 길이와 같다면 문자열을 뒤집는 함수 revert_Str())을 실행
+    //같지 않다면 예외처리 문장을 출력하고 프로그램 종료 
 }
 
 void revert_Str(char* rev_Str){
@@ -40,7 +48,9 @@ void revert_Str(char* rev_Str){
     char tmp;
     start = rev_Str;
     end = rev_Str + strlen(rev_Str) - 1;
-    
+    //포인터 각각 선언하고 초기화
+    //서로 값을 바꿀때 임시로 저장할 변수 선언
+    //포인터의 초기화는 문자열 처음 위치와 문자열의 마지막 위치
     for(int i = 0; i < strlen(rev_Str)/2; i++){
         tmp = *(start);
         *(start) = *(end);
@@ -48,6 +58,10 @@ void revert_Str(char* rev_Str){
         *(start)++;
         *(end)--;
     }
+    //tmp에 start값을 저장, start에 end값을 저장
+    //end에 tmp값을 저장
+    //strat의 주소를 +1, end의 주소를 -1 계산
+    //안쪽의 글자를 뒤집음으로서 문자열의 모든 문자를 뒤집음
     
     printf("뒤집어진 문자열 : %s", rev_Str, sizeof(rev_Str));
     printf("\n");
--- day08/day08-2.c
+++ day08/day08-2.c
@@ -20,14 +20,15 @@ int main(){
 
 void fillRandom(int array[SIZE][SIZE]){
     int random;
-    srand(time(NULL));
+    srand(time(NULL)); //
     
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             random = rand() % 20 + 1 ;
             array[i][j] = random;
         }
-    } 
+    }
+    //1~20사이의 랜덤값을 배열에 대입
 }
 
 void printArray(int array[SIZE][SIZE]){
@@ -38,26 +39,34 @@ void printArray(int array[SIZE][SIZE]){
         printf("\n");
     } 
 }
+//배열의 값을 3자리수 형태로 출력
 
 void movePointer(void* array){
     int x = 0, y = 0, val, n, nn;
     void* cur = array;
+    //현재 위치의 주소를 저장할 포인터 변수 선언
     
     while(1){    
         val = *((int*)cur);
         cur=(int*)cur + val;
+        //val에 현재 값을 대입
+        //cur의 위치를 val만큼 이동
        
         printf("현재 위치 : (%d, %d), 배열의 값 : %d\n", x, y, val);
         y = y + val; 
+        //y가 val값 만큼 증가
         if(x == 8 && y >= 20 || x == 9 && y >= 10){
             y = y - val;
             break;
         }
+        //좌표 값을 표시해주는 값이 배열의 범위를 넘어가지 않게끔 반복문 정지
         if(y > 9 && x < 9){
             x = x + y / 10;
             y = y % 10;
         }
-        
+        //x에 y를 10으로 나눈 값(10의 자리)
+        //y에 y를 10으로 나눈 나머지 (1의 자리) 대입
+
     }
     printf("\n");
     printf("더 이상 이동 할 수 없습니다. \n");
