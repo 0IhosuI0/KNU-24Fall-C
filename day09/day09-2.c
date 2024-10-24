84608c6e3da77384fa1bf4918909809b526e4c26
hosu10 <ghtn10000@naver.com>
2024-10-24 17:18:26 +0900

    ver 1.1

--- day09/day09-2.c
+++ day09/day09-2.c
@@ -11,6 +11,7 @@ void printProduct(struct Product Product);
 int main(){
     int cnt = 0;
     struct Product Products[5];
+    //구조체 배열 선언
     for(int i = 0; i < 5; i++){
         printf("상품 정보를 입력하세요 (입력 중단은 ID에 0 입력)\n");
         printf("상풍 ID : ");
@@ -18,6 +19,7 @@ int main(){
         if (Products[i].ID == 0){
              break;
         }
+        //구조체의 ID부분에 0이 입력되면 반복문에서 빠져나옴
         cnt++;
         printf("상품명 : ");
         scanf("%s", Products[i].Product_Name);
@@ -29,6 +31,8 @@ int main(){
     for(int j = 0; j < cnt; j++){
         printProduct(Products[j]);
     }
+    //Product 배열의 j번째 정보를 인수로 printProduct를 선언
+    //한개의 인수로 입력한 횟수만큼 출력할 수 있도록 인수를 보내주고 출력함  
     
     return 0;
 }
@@ -38,3 +42,4 @@ void printProduct(struct Product Product){
         printf("상품명 : %s\n", Product.Product_Name, 100);
         printf("가격 : %d원\n\n", Product.cost);
 }
+//배열의 j번째 정보를 받고 출력
